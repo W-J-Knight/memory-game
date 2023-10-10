40be800bd5a89b96bdc54fb707d43f840247d4f7
W-J-Knight <William.Jeffery.Knight@protnmail.com>
2023-10-10 08:53:13 -0400

    styled the home a little

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useGlobalContext } from "../../context/cardDeckContext";
 import ShuffleCards from "../../hooks/ShuffleCards";
+import styles from "./Home.module.css"
 const Home = () => {
   const { shuffledCards, setShuffledCards, cardsIcons } = useGlobalContext();
   const newGame = () => {
@@ -8,7 +9,7 @@ const Home = () => {
   // console.log(cardsIcons);
   console.log(shuffledCards);
   return (
-    <>
+    <section>
       <h1>Home</h1>
       <button
         onClick={() => {
@@ -17,7 +18,7 @@ const Home = () => {
       >
         New Game
       </button>
-    </>
+    </section>
   );
 };
 
